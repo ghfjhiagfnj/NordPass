186cc0782cbbc46fdc29cf2c7ad86da4afcdb36c
ghfjhiagfnj <ghfjhiagfnj@outlook.com>
2026-08-06 12:35:18 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+project = 'NordPass-login'
+author = 'NordPass-login'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
